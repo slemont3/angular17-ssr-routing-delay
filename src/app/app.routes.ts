541a4dd2affebcdd2661b5d33b97812f9044a53b
simonfankhauser <simon.fankhauser@youengineering.ch>
2024-04-25 11:39:56 +0300

    update average loading times after further testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,11 @@ import {WebsiteComponent} from "./website/website.component";
 
 export const routes: Routes = [
 
-  // Average initial loading time around 15 - 20ms
+  // Average initial loading time around 15ms
   {
     path: 'test', component: WebsiteComponent
   },
-  // Wildcard route seems to be the core of the problem: Average initial loading time around 250 - 300ms
+  // Wildcard route seems to be the core of the problem: Average initial loading time around 300ms
   {
     path: '**', component: WebsiteComponent
   },
